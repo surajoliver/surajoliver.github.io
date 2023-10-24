d1047e1d392e28ec5dd0e9308295343ffc6004a8
surajoliver <suraj.oliver@gmail.com>
2023-10-24 10:59:58 +0530

    reorganized folders

--- tw_templates/twtemplate_band/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  // content: ["./src/index.html"],
-  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
